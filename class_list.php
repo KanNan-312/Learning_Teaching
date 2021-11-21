b9819a2281579e8bf86fb42c7015b7e5156fb8db
Khang Ho <htkhang0966@gmail.com>
2021-11-21 14:00:44 +0700

    Error in class_list

--- class_list.php
+++ class_list.php
@@ -1,4 +1,33 @@
 <div class="course-container">
+    <?php
+    $code = $_GET['code'];
+    ?>
+    <p style="font-size: 30px;">Lecturer list</p>
+	<table id="table">
+        <tr>
+            <th>Name</th>
+            <th>Teacher ID</th>
+            <th>Role</th>
+        </tr>
+        <?php
+            
+            $sql = "call showTeacherList('$code');";
+            $result = $conn -> query($sql);
+            if ($result->num_rows > 0) {
+                while($row = $result -> fetch_assoc())
+                {
+                    echo "
+                    <tr>
+                        <td>".$row['Name']."</td>
+                        <td>".$row['ID']."</td>
+                        <td>".$row['Role']."</td>
+                    </tr>
+                    ";
+                }
+            }
+        ?>
+        <hr>
+    </table>
 	<p style="font-size: 30px;">Student list</p>
 	<table id="table">
         <tr>
@@ -7,9 +36,11 @@
             <th>Email</th>
         </tr>
         <?php
-            $code = $_GET['code'];
             $sql = "call showStudentList('$code');";
             $result = $conn -> query($sql);
+            echo $sql;
+            if (!$result)
+                die(!$result->error);
             if ($result->num_rows > 0) {
                 while($row = $result -> fetch_assoc())
                 {
